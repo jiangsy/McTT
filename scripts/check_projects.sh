6e3276a21aa37d7e40524edf5ec333b1d5edcc70
Junyoung/Clare Jang <jjc9310@gmail.com>
2025-09-05 15:05:56 -0400

    Fix check_projects script

--- scripts/check_projects.sh
+++ scripts/check_projects.sh
@@ -3,15 +3,16 @@
 dir=${1?directory unspecified}
 
 error_out () {
-
-    echo "$1 is not in the _CoqProject file" >&2
+    local file_path=$(realpath "$1")
+    local coq_project_path=$(realpath "_CoqProject")
+    echo "${file_path} is not in the ${coq_project_path} file" >&2
     exit 1
 }
 
 cd "${dir}"
 
 for f in `find -name '*.v'`; do
-    grep -q -F "^${f}" _CoqProject || error_out "${f}"
+    grep -qxF "${f}" _CoqProject || error_out "${f}"
 done
 
 exit 0
